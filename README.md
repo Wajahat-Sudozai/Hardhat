3864f420089be6842c13a3376373bc3f81f82570
wajahat8888 <97318378+wajahat8888@users.noreply.github.com>
2022-10-27 13:26:52 +0500

    Hardhat Script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Hardhat
+# Sample Hardhat Project
+
+This project demonstrates a Hardhat use case . It comes with a sample contract, a test for that contract, a script that deploys that contract , a script that verify contract on different network , a gas report generator, and a contract testing coverage details.
+
+Try running some of the following tasks:
+
+```shell
+npm install
+npx hardhat verify --list-networks
+npx hardhat test
+npx hardhat test test/ERC721_Test.js
+npx hardhat run scripts/deploy.js
+npx hardhat run scripts/deploy.js --network polygonTestnet
+REPORT_GAS=true npx hardhat test
+```
---
+++ scripts/deploy.js
@@ -0,0 +1,50 @@
+const { ethers , run,network} = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log(`Deploying contracts with the account: ${deployer.address}`);
+  //ERC721 Contract Deploying
+  const ERC721 = await ethers.getContractFactory("ERC721Test");
+  const ERC721Contract = await ERC721.deploy();
+  await ERC721Contract.deployed();
+  console.log(`ERC721 Contract deployed to ${ERC721Contract.address}`)
+  //ERC721 Contract verification
+  if(network.config.chainId===97&& process.env.BINANCE_MAINNET_API ){
+    await ERC721Contract.deployTransaction.wait(6);
+    console.log("ERC721 Contract Verification on Binance Network");
+    await verify(ERC721Contract.address,[])
+  }
+  else if(network.config.chainId===80001&& process.env.POLYGON_MAINNET_API){
+    await ERC721Contract.deployTransaction.wait(6);
+    console.log("ERC721 Contract Verification on Polygon Network");
+    await verify(ERC721Contract.address,[])
+  }
+  else if(network.config.chainId===5&& process.env.ETHEREUM_MAINET_API){
+    await ERC721Contract.deployTransaction.wait(6);
+    console.log("ERC721 Contract Verification on Goerli Network");
+    await verify(ERC721Contract.address,[])
+  }
+}
+
+async function verify (contractAddress,args){
+  console.log("verifying contract....")
+  try{
+  await run("verify:verify", {
+    address: contractAddress,
+    constructorArguments: args,
+  });
+ }catch(e)
+ {
+  if(e.message.toLowerCase().includes("already verified")){
+    console.log("Already verified!");
+  }else{
+  console.log(e)
+  }
+ }
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
